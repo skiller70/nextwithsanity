498b9d1c4582aed0492695bc40da5a7bfee9e994
skiller70 <singhlucky007007@gmail.com>
2023-11-05 14:21:10 +0530

    removed navbar from get all post

--- app/getpost/[allpost]/page.jsx
+++ app/getpost/[allpost]/page.jsx
@@ -1,18 +1,10 @@
-import Navbar from "@/app/components/Navbar";
 import React from "react";
 
 export default async function page({ params }) {
-    
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.allpost}`
   );
   const data = await res.json();
 
-  return <>
-  <Navbar/>
-  {data.title}
-  
-  
-  
-  </>;
+  return <>{data.title}</>;
 }
